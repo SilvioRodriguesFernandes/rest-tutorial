f9650156676686ecdff95eb52a7fd0d92bc6e278
Silvio Rodrigues Fernandes <cleniolelys@gmail.com>
2023-06-01 22:51:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middlewares
+app.use(cors());
+
+// Import routes
+const postsRouter = require('./routes/posts');
+
+app.use('/posts', postsRouter);
+
+// Connect to DB
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    app.emit('Ready');
+    console.log(
+      'Connected to DB:',
+      mongoose.connection.host,
+      mongoose.connection.name
+    );
+  })
+  .catch((err) => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to our Home!!!');
+});
+
+// How to we start listening to the server
+app.on('Ready', () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Access:http://localhost:${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const Post = require('../models/Post');
+
+// Get back all the posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Submit a post
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Specific post
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Delete specific post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndRemove({
+      _id: req.params.postId,
+    });
+    res.json(deletedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Update a post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+module.exports = router;
