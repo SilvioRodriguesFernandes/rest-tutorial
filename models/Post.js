1f356faf74816adb1e28298ebffdce68873f38f3
Silvio Rodrigues Fernandes <cleniolelys@gmail.com>
2023-06-07 21:04:51 -0400

    Update model

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
-const PostSchema = mongoose.Schema({
+const PostSchema = new Schema({
   title: {
     type: String,
     required: true,
